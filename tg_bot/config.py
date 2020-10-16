ab50bb1a71c6c98a666552fca4a62068df646107
gym2105 <69361655+gym2105@users.noreply.github.com>
2020-10-16 06:57:19 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -8,6 +8,6 @@ class Development(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgresgym:21052001@localhost:5432/database-1'  # sample db credentials
     MESSAGE_DUMP = '1001221946970' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
-    SUDO_USERS = [1261080659]  # List of id's for users which have sudo access to the bot.
+    SUDO_USERS = [1208721971]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['translation']
