f7f3524661ab7e518edfc63f5ac739b8fa767f87
gym2105 <69361655+gym2105@users.noreply.github.com>
2020-10-17 14:07:58 +0530

    Update filters.py

--- tg_bot/modules/helper_funcs/filters.py
+++ tg_bot/modules/helper_funcs/filters.py
@@ -5,11 +5,7 @@ from tg_bot import SUPPORT_USERS, SUDO_USERS
 
 
 class CustomFilters(object):
-    class _Supporters(BaseFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
-
-    support_filter = _Supporters()
+    
 
     class _Sudoers(BaseFilter):
         def filter(self, message: Message):
